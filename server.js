102820eccf9fde970e82531b25600476cc3e30df
mvpache <mvpache@msn.com>
2018-05-02 15:27:57 -0700

    mongoose connection to mlab

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require('express');
-
 const PORT = process.env.PORT || 5000;
+const config = {
+  dbuser: process.env.dbuser,
+  dpass: process.env.dbpassword,
+};
 const server = express();
+
+mongoose
+  .connect(
+    `mongodb://${config.dbuser}:${
+      config.dpass
+    }@ds113736.mlab.com:13736/lambdanotes`
+  )
+  .then(cnn => {
+    console.log('\n=== connected to mongo ===\n');
+  })
+  .catch(err => {
+    console.log('\n=== ERROR connecting to mongo ===\n');
+  });
 
 server.get('/', (req, res) => {
   res.send({ api: 'up and running' });
